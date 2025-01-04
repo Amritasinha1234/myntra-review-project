64f012c499c7592be0d90280fd75943e52899cc6
Amritasinha1234 <amrita.sinha23-25@bibs.co.in>
2025-01-04 19:21:46 +0530

    commit

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://amritasinha2325:5ecDjx0yRiWyxutJ@cluster0.or3ag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
