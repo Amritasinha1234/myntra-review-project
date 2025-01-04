75ec2e979d964b7f978172515022aa43a50f7a2f
Amritasinha1234 <amrita.sinha23-25@bibs.co.in>
2025-01-04 08:55:43 +0530

    this is my 1st commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='a_sinha',
+    author_email='amrita.sinha23-25@bibs.co.in',
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
